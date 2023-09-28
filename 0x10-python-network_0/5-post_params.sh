cf644ef180bcb885d5af0c608f86732b895a3782
Iamvkyroy <itavkyroy@gmail.com>
2023-09-28 09:17:49 -0700

    0x10. Python - Network #0

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,28 +1,3 @@
 #!/bin/bash
-
-# Check if a URL is provided as an argument
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# URL to send the POST request to
-URL="$1"
-
-# Variables to send in the POST request
-email="test@gmail.com"
-subject="I will always be here for PLD"
-
-# Send the POST request using curl
-response=$(curl -s -X POST "$URL" \
-  -d "email=$email" \
-  -d "subject=$subject")
-
-# Check if curl was successful
-if [ $? -eq 0 ]; then
-  echo "Response body:"
-  echo "$response"
-else
-  echo "Failed to send POST request to $URL"
-  exit 1
-fi
+# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -s "$1" -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
